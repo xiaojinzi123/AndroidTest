cbc0e7cbf363d9718b65f5ac030897d354dc6aac
xiaojinzi <347837667@qq.com>
2018-06-02 18:55:43 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaojinzi/test/MainAct.kt
@@ -0,0 +1,12 @@
+package com.xiaojinzi.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_act)
+    }
+}
