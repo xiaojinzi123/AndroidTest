551dfd13a3d5f83ddf96421ccdb02ff00ab5cb1c
xiaojinzi <347837667@qq.com>
2018-06-03 18:15:03 +0800

    qwe123123

--- app/src/main/java/com/xiaojinzi/test/MainAct.kt
+++ app/src/main/java/com/xiaojinzi/test/MainAct.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 
 class MainAct : AppCompatActivity() {
 
+    // eh_ic_balance_no_record
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_act)
